9fe9abc956228c9a3c6334ddba07d3ef75831973
Initlipton <gerryjlawless@gmail.com>
2015-04-28 11:26:35 +0100

    First Commit
    First Commit

---
+++ ClearableEditView/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ClearableEditView
+{
+    [Activity(Label = "ClearableEditView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IListener
+    {
+        private ClearableEditText textEdit;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            textEdit = FindViewById<ClearableEditText>(Resource.Id.searchText);
+            textEdit.SetListener(this);
+        }
+
+        public void DidClearTextBox()
+        {
+            textEdit.Text = string.Empty;
+        }
+    }
+}
+
